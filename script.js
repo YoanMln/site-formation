3050dd86df2996cf357a6d9fe6ca21c453ca9d43
YoanMln <yoan.moulin@hotmail.fr>
2025-04-30 16:14:09 +0200

    Ajout partie météo, création menu drop down

--- script.js
+++ script.js
@@ -30,3 +30,35 @@ function loadNextPoke() {
 
 loadNextPoke();
 nextBtn.addEventListener(`click`, loadNextPoke);
+
+//-----Météo-----//
+
+function getWeather() {
+  const location = document.getElementById("location").value;
+  const apiKey = "35b98ed6f0e852ba49933ec13c51d26e";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const { temp, feels_like } = data.main;
+      const description = data.weather[0].description;
+      const cityName = data.name;
+      const country = data.sys.country;
+
+      const weatherDisplay = `Current weather in ${cityName}, ${country}: ${description}, ${temp}°C (feels like ${feels_like}°C)`;
+
+      document.getElementById("weather").textContent = weatherDisplay;
+    })
+    .catch((error) => {
+      console.error(`Erreur :`, error);
+    });
+}
+
+//-----DropDownMenu-----//
+
+const button = document.getElementById("dropDownMenuButton");
+const menu = document.getElementById(`dropDownMenu`);
+
+button.addEventListener(`click`, () => {
+  menu.classList.toggle(`show`);
+});
